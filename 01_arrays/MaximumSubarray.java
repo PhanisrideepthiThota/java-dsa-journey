e24150b65647dfb9d7530afc6ae6bc285c923deb
DEEPTHI <phanisrideepthi@gmail.com>
2026-09-11 11:59:24 +0530

    Add MaximumSubarray class with maxSubArray method
    Implement the Maximum Subarray problem using Kadane's algorithm.

---
+++ 01_arrays/MaximumSubarray.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] arr) {
+
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(maxSubArray(arr));
+
+        sc.close();
+    }
+}
